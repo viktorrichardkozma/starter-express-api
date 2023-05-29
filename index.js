d5e65e82f9eb8c28d244e98113d66ae18317d3a8
Viktor Kozma <kozma.viktor.richard@gmail.com>
2023-05-30 00:58:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import express from "express";
 import moment from "moment"
 import {google} from 'googleapis';
+import {getColumnValues} from './spreadsheet.js'
 
+/* import {sendWelcomeMail} from "./mailservice.js";
+ */
 const app = express();
-
-import dotenv from 'dotenv'
-dotenv.config()
 
 // Provide the required configuration
 const client_email = process.env.CLIENT_EMAIL;
 const calendarId = process.env.CALENDAR_ID;
-
 const fixedKey = process.env.KEY.replace(new RegExp("\\\\n", "\g"), "\n")
 
 // Google calendar API settings
@@ -55,13 +57,26 @@ app.get('/'+process.env.ENDPOINT, (req, res) => {
   getTodayEvents().then( events => {
 
     if(events.length>0){
-      const summaries = events.map( ({summary}) => summary)
-      res.send(summaries.join(', '))
 
+      const { summary } = events[0];
+      const { start : {date : startDate}  = {} } = events[0];
+      const { end : {date : endDate} = {}} = events[0];
+
+      getColumnValues(moment(startDate, 'YYYY-MM-DD'),moment(endDate, 'YYYY-MM-DD')).then( guests =>
+        
+        res.send(JSON.stringify({
+          summary,
+          startDate,
+          endDate,
+          guests
+        },null, 2))  
+        
+      );
     }
   })
 
 })
+
 
 app.listen(3000, () => {
   console.log("application listening.....");
