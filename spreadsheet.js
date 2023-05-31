666e1931f5333f695836ab6e591308053d9bc5f1
Viktor Kozma <kozma.viktor.richard@gmail.com>
2023-06-01 00:18:12 +0200

    Update spreadsheet.js

--- spreadsheet.js
+++ spreadsheet.js
@@ -6,45 +6,29 @@ dotenv.config()
 
 const client_email = process.env.CLIENT_EMAIL_XLS;
 const spreadsheetId = process.env.SPREADSHEET_ID;
-const fixedKey = process.env.PRIVATE_KEY.replace(new RegExp("\\\\n", "\g"), "\n")
 
 async function getColumnValues(startDate, endDate) {
 
 const formattedStartDate = moment(startDate, 'YYYY-MM-DD')
 const formattedEndDate = moment(endDate, 'YYYY-MM-DD')
-const credentials = {
-    "type": process.env.TYPE,
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
-    "client_email": process.env.CLIENT_EMAIL_XLS,
-    "client_id": process.env.CLIENT_ID,
-    "auth_uri": process.env.AUTH_URI,
-    "token_uri": process.env.TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.UNIVERSE_DOMAIN
-}
 
-const fixedKey = process.env.PRIVATE_KEY.replace(new RegExp("\\\\n", "\g"), "\n")
+const fixedKey = (process.env.KEY_XLS_FIRST + process.env.KEY_XLS_SECOND).replace(new RegExp("\\\\n", "\g"), "\n")
 
-    const client = new google.auth.JWT(
-        process.env.CLIENT_EMAIL_XLS,
-        null,
-        fixedKey,
-        ['https://www.googleapis.com/auth/spreadsheets']
-    );
+const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.CLIENT_EMAIL_XLS,
+      private_key: fixedKey,
+    },
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
 
-    await client.authorize((err, tokens) => {
-      
-            google.options({
-                auth: client
-            });
-        
-    });
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google Sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
 
      // Instance of Google Sheets API
-     const googleSheets = google.sheets({ version: "v4", auth: client });
 
   try {
         // Read rows from spreadsheet
